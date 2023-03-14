b33ffba459344236d13586f02a15d7c88f513e5a
unknown <alelopmay@gmail.com>
2023-03-14 09:27:42 +0100

    primeros datos

--- src/main/java/model/DTO/Gimnast.java
+++ src/main/java/model/DTO/Gimnast.java
@@ -1,5 +1,39 @@
 package model.DTO;
 
 public class Gimnast extends Person {
+	private String Categori;
+	 private String Club;
+
+	public Gimnast(String dNI, String name, String email, String phoneNumber, String categori, String club) {
+		super(dNI, name, email, phoneNumber);
+		Categori = categori;
+		Club = club;
+	}
+	
+
+	public String getCategori() {
+		return Categori;
+	}
+
+
+	public void setCategori(String categori) {
+		Categori = categori;
+	}
+
+
+	public String getClub() {
+		return Club;
+	}
+
+
+	public void setClub(String club) {
+		Club = club;
+	}
+
+
+	@Override
+	public String toString() {
+		return super.toString()+"Categori=" + Categori + ", Club=" + Club + "]";
+	}
 
 }
