ec3692487f620cd5d674039555584088563be85c
adrian <adrian4rs@gmail.com>
2023-03-14 09:52:13 +0100

    Primerasubida

--- src/main/java/model/Group.java
+++ src/main/java/model/Group.java
@@ -1,7 +1,53 @@
 package model;
 
 import interfaces.iGroup;
+import model.DTO.Gimnast;
 
 public class Group implements iGroup {
+	private String nombre;
+	private String club;
+	
+	public Group(String nombre, String club) {
+		super();
+		this.nombre = nombre;
+		this.club = club;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getClub() {
+		return club;
+	}
+
+	public void setClub(String club) {
+		this.club = club;
+	}
+
+	public boolean gimnastasCompatibles(Gimnast gimnast) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void modificarGrupo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean insertarGimnasta(Gimnast gimnast) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Gimnast borrarGimnasta(Gimnast gimnast) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
 }
--- src/main/java/model/Trial.java
+++ src/main/java/model/Trial.java
@@ -1,7 +1,64 @@
 package model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import interfaces.iTrial;
 
 public class Trial implements iTrial {
+	private String nombre;
+    private Set<Entry> participaciones;
+    
+	public Trial(String nombre, Set<Entry> participaciones) {
+		super();
+		this.nombre = nombre;
+		this.participaciones = new HashSet<Entry>();
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Set<Entry> getParticipaciones() {
+		return participaciones;
+	}
+
+	public void setParticipaciones(Set<Entry> participaciones) {
+		this.participaciones = participaciones;
+	}
+	
+	
+	public Trial modificarPrueba() {
+		return null;
+	}
+    
+    
+    public Entry buscarParticipacion(int number) {
+		return null;
+    	
+    }
+    
+    public Entry getWinner() {
+		return null;
+    	
+    }
+    
+    public boolean insertarParticipacion(Entry entry) {
+		return false;
+    	
+    }
+    
+    public void mostrarParticipaciones() {
+    	
+    }
+    
+    public boolean horaDuplicada(String hora) {
+		return false;
+    	
+    }
+	
 }
